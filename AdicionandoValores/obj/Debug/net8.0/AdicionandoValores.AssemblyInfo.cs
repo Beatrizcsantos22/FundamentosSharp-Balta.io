26edb924dbe1eaaa47790bf48ac42a1d956af136
Beatriz <beatrizcaroline005@gmail.com>
2024-08-29 14:53:28 -0300

    first commit

--- AdicionandoValores/obj/Debug/net8.0/AdicionandoValores.AssemblyInfo.cs
+++ AdicionandoValores/obj/Debug/net8.0/AdicionandoValores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdicionandoValores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3a65f758ca0be28d95f3735720f9c63b00db7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdicionandoValores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdicionandoValores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
